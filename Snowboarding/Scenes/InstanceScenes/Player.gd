3b50a8db3511a07d700fde442a19a10afa7bd452
StormFalcon32 <benchen9708@gmail.com>
2021-09-26 12:58:48 -0500

    Basic procedural gen and working on flags

--- Snowboarding/Scenes/InstanceScenes/Player.gd
+++ Snowboarding/Scenes/InstanceScenes/Player.gd
@@ -3,25 +3,37 @@ extends KinematicBody
 # Physical characteristics
 export var slope_angle: float = -PI / 6
 export var gravity: float = 9.8
-export var friction: float = 2
-export var initial_vel: float = 1
+export var initial_vel: float = 2
 export var min_vel: float = 0.5
-export var max_angle_diff: float = 2 * PI / 5 
+export var max_angle_diff: float = 2 * PI / 5
+export var min_friction: float = 0.0
+export var max_friction: float = 0.1
+
 export var turn_vel_coeff: float = 0.5
+export var rtc_vel_coeff: float = 0.5
 export var angle_vel_coeff: float = 0.6
+export var friction_vel_coeff: float = 0.00002
 
 var downhill: Vector3 = Vector3.FORWARD.rotated(Vector3.RIGHT, slope_angle).normalized()
 var normal: Vector3 = Vector3.RIGHT.cross(downhill).normalized()
 
 # Vel
 var velocity: Vector3 = downhill * initial_vel
+var downhill_vel: Vector3 = downhill * initial_vel
+var lateral_vel: Vector3 = Vector3()
 
-func _ready():
+# Death
+signal death
+
+func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
-func _physics_process(delta: float):
+func _physics_process(delta: float) -> void:
 	# Check angle difference between current velocity and downhill
 	var angle_diff: float = downhill.angle_to(velocity)
+	# Get signed difference
+	if downhill.cross(velocity).dot(normal) < 0:
+		angle_diff = -angle_diff
 	# Inverted angle difference
 	var inv_angle_diff: float = PI / 2 - abs(angle_diff)
 	# Turn player if angle difference doesn't exceed max
@@ -29,12 +41,19 @@ func _physics_process(delta: float):
 		velocity = velocity.rotated(normal, PI * inv_angle_diff * turn_vel_coeff * delta)
 	if Input.is_action_pressed('move_right') and angle_diff > -max_angle_diff:
 		velocity = velocity.rotated(normal, -PI * inv_angle_diff * turn_vel_coeff * delta)
-#	Give more forward velocity the more the player faces downhill
+	# Return to center force
+	velocity = velocity.rotated(normal, -PI * inv_angle_diff * turn_vel_coeff * angle_diff * rtc_vel_coeff * delta)
+	# Give more forward velocity the more the player faces downhill
 	var normalized_vel: Vector3 = velocity.normalized()
 	velocity += normalized_vel * gravity * pow(inv_angle_diff, 2) * angle_vel_coeff * delta
-	velocity -= normalized_vel * friction * delta
+	# Apply friction based on speed
+	velocity *= 1 - clamp(velocity.length() * friction_vel_coeff, min_friction, max_friction)
 	if velocity.length() < min_vel:
 		velocity = normalized_vel * min_vel
-	var move_collision: KinematicCollision = move_and_collide(velocity * delta)
+	# Break velocity down into downhill and lateral components
+	downhill_vel = velocity.project(downhill)
+	lateral_vel = velocity - downhill_vel
+	# Only move player along lateral velocity, downhill velocity will be simulated by moving obstacles uphill
+	var move_collision: KinematicCollision = move_and_collide(lateral_vel * delta)
 	if move_collision:
-		queue_free()
+		emit_signal('death')
--- Snowboarding/Scenes/RunnableScenes/Main.gd
+++ Snowboarding/Scenes/RunnableScenes/Main.gd
@@ -1,10 +1,115 @@
 extends Spatial
 
+# Nodes
+onready var player = $Player
+
+# Procedural gen
+export var first_rock_dist: int = 0
+export var min_rock_dist: int = 20
+export var max_rock_dist: int = 80
+
+export var first_flag_dist: int = 30
+export var min_flag_dist: int = 100
+export var max_flag_dist: int = 200
+
+export var min_horiz: int = -80
+export var max_horiz: int = 80
+
+export var obstacle_offset_dist: int = 200
+export var obstacle_delete_dist: int = 100
+
+var rand = RandomNumberGenerator.new()
+
+var rock_scene: PackedScene = preload('res://Scenes/InstanceScenes/Rock.tscn')
+var rocks: Array = []
+var next_rock_dist: int = first_rock_dist
+
+var flag_scene: PackedScene = preload('res://Scenes/InstanceScenes/FlagPair.tscn')
+var flags: Array = []
+var next_flag_dist: int = first_flag_dist
+
+var dist_traveled: float = 0
+
 # Debug vars
 export var camera: int = 0
+export var debug_freq: int = 600
+var debug_frame: int = 0
 
-func _ready():
+func _ready() -> void:
+	player.connect('death', self, '_on_player_death')
+	rand.randomize()
 	if camera == 0:
 		$Player/Camera.current = true
 	elif camera == 1:
 		$Camera.current = true
+
+func _on_player_death() -> void:
+	# Stop everything
+	print('Dead')
+	player.velocity = Vector3()
+	for rock in rocks:
+		rock.velocity = Vector3()
+	for flag in flags:
+		flag.velocity = Vector3()
+
+func _process(delta: float) -> void:
+	# Check player hasn't died yet
+	if !is_instance_valid(player):
+		return
+	# Keep track of distance traveled
+	dist_traveled += player.downhill_vel.length() * delta
+	# Once enough distance has been traveled, generate new rock
+	if dist_traveled >= next_rock_dist:
+		var rock: Spatial = rock_scene.instance()
+		# Generate out of view using offset
+		rock.transform.origin = player.downhill * obstacle_offset_dist
+		# Give random horizontal offset
+		rock.transform.origin += Vector3.RIGHT * rand.randi_range(min_horiz, max_horiz)
+		# Finish generating
+		rock.velocity = -player.downhill_vel
+		rock.connect('death', self, '_on_player_death')
+		rocks.append(rock)
+		add_child(rock)
+		# Calculate where to generate next rock
+		next_rock_dist = dist_traveled + rand.randi_range(min_rock_dist, max_rock_dist)
+		print('Next rock: ' + str(next_rock_dist))
+	# Do similar for flag
+	if dist_traveled >= next_flag_dist:
+		var flag: Spatial = flag_scene.instance()
+		flag.transform.origin = player.downhill * obstacle_offset_dist
+		flag.transform.origin += Vector3.RIGHT * rand.randi_range(min_horiz, max_horiz)
+		flag.velocity = -player.downhill_vel
+		flag.connect('death', self, '_on_player_death')
+		flags.append(flag)
+		add_child(flag)
+		next_flag_dist = dist_traveled + rand.randi_range(min_flag_dist, max_flag_dist)
+		print('Next flag: ' + str(next_flag_dist))
+	# Update rock velocities to match current player velocity and delete any that are out of view
+	var rocks_to_delete: Array = []
+	for rock in rocks:
+		rock.velocity = -player.downhill_vel
+		# Check if rock is uphill of obstacle delete dist
+		if (-player.downhill * obstacle_delete_dist).z - rock.transform.origin.z < 0:
+			rocks_to_delete.append(rock)
+	for rock in rocks_to_delete:
+		rocks.erase(rock)
+		rock.queue_free()
+	# Do similar for flag
+	var flags_to_delete: Array = []
+	for flag in flags:
+		flag.velocity = -player.downhill_vel
+		print((-player.downhill * obstacle_delete_dist).z)
+		print(flag.transform.origin.z)
+		if (-player.downhill * obstacle_delete_dist).z - flag.transform.origin.z < 0:
+			flags_to_delete.append(flag)
+	for flag in flags_to_delete:
+		flags.erase(flag)
+		flag.queue_free()
+		print('Flag deleted')
+#	debug_frame += 1
+#	if debug_frame == debug_freq:
+#		debug_frame = 0
+#		print('Num rocks: ' + str(rocks.size()))
+#		print('Num flags: ' + str(flags.size()))
+#		print('----')
+	
